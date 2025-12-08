6c0d522bee74e55c821369a4bfaf82c2c1618f37
JH0908 <mashifed@gmail.com>
2025-12-08 14:38:21 +0900

    style: adjust icon size from 20px to 16px

--- front-end/src/pages/board/List.tsx
+++ front-end/src/pages/board/List.tsx
@@ -46,11 +46,11 @@ export default function List() {
             </table>
             <div className='pagination'>
                 <a href='' className='page-arrow first'>
-                    <Icon name="angleDoubleLeft" width={20} height={20} fill="#333" />
+                    <Icon name="angleDoubleLeft" width={16} height={16} fill="#333" />
                     <span>처음 페이지로 이동</span>
                 </a>
                 <a href='' className='page-arrow prev'>
-                    <Icon name="angleLeft" width={20} height={20} fill="#333" />
+                    <Icon name="angleLeft" width={16} height={16} fill="#333" />
                     <span>이전 페이지로 이동</span>
                 </a>
                 <button type='button' className='current-page'><span>1</span></button>
@@ -59,11 +59,11 @@ export default function List() {
                 <a href=''><span>4</span></a>
                 <a href=''><span>5</span></a>
                 <a href='' className='page-arrow next'>
-                    <Icon name="angleRight" width={20} height={20} fill="#333" />
+                    <Icon name="angleRight" width={16} height={16} fill="#333" />
                     <span>다음 페이지로 이동</span>
                 </a>
                 <a href='' className='page-arrow last'>
-                    <Icon name="angleDoubleRight" width={20} height={20} fill="#333" />
+                    <Icon name="angleDoubleRight" width={16} height={16} fill="#333" />
                     <span>마지막 페이지로 이동</span>
                 </a>
             </div>
