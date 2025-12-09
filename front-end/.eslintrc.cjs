613aa532c8975c3059f86dd9f2a4f76d03ec2774
jh <jhhn0908@gmail.com>
2025-12-10 00:11:27 +0900

    ci: WIP - initial GitHub Actions CI setup (not completed yet)

---
+++ front-end/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {jsx: true}
+    },
+    settings: {
+        react: {version: "detect"}
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    plugins: ["react", "react-hooks", "@typescript-eslint", "import"],
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["warn"]
+    }
+}
---
+++ front-end/src/App.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+test('simple test', () => {
+    expect(1 + 1).toBe(2)
+})
