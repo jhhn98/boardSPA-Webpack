68e4b21d47fd9603a64980888526196b6213a8c8
jh <jhhn0908@gmail.com>
2025-12-15 22:28:03 +0900

    feat(form): WIP – start custom design for input[type="file"]

--- front-end/src/pages/board/Write.tsx
+++ front-end/src/pages/board/Write.tsx
@@ -50,17 +50,61 @@ export default function Write() {
                                  두번째 입력란부터는 +, - 버튼 추가
                                  입력란 추가는 최대 10개까지 기본값으로 최대갯수는 옵션으로 받아 설정할 수 있음.
                                  */}
-                                <div className="form-attachment">
-                                    <div className="form-element">
-                                        <label htmlFor="file1">File1</label>
-                                        <input type="file" id="file1" className="input-file" />
-                                        <button type="button">+</button>
-                                    </div>
-                                    <div className="form-element">
-                                        <label htmlFor="file2">File2</label>
-                                        <input type="file" id="file2" className="input-file" />
-                                        <button type="button">+</button>
-                                        <button type="button">-</button>
+                                <div className="form-element custom-input-file">
+                                    <div className="file-picker">
+                                        <div className="file-dropzone">
+                                            <p className="file-dropzone-comment">파일을 여기로 드래그하거나</p>
+                                            <label htmlFor="file1" className="file-dropzone-pick">
+                                                <span>파일 선택</span>
+                                                <input type="file" id="file1" className="input-file" multiple />
+                                            </label>
+                                            <p className="file-dropzone-pick-comment">여러 파일 선택 가능</p>
+                                        </div>
+                                        <div className="file-queue">
+                                            <ul className="file-queue-list">
+                                                <li className="file-queue-item">
+                                                    <span className="name">
+                                                        <em className="file-name">aasdfasdfasdfasdfasdfqwerdgkasjefhqpwoeiufpisudhfkqwjehrlksjdhf</em>
+                                                        <em className="extension">.html</em>
+                                                    </span>
+                                                    <span className="meta">12 KB</span>
+                                                    <button type="button" className="file-item-remove">
+                                                        <span>파일 제거</span>
+                                                        <Icon name="crossSmall" width={20} height={20} />
+                                                    </button>
+                                                </li>
+                                                <li className="file-queue-item">
+                                                    <span className="name">
+                                                        <em className="file-name">a</em>
+                                                        <em className="extension">.html</em>
+                                                    </span>
+                                                    <span className="meta">12 KB</span>
+                                                    <button type="button" className="file-item-remove">
+                                                        <span>파일 제거</span>
+                                                        <Icon name="crossSmall" width={20} height={20} />
+                                                    </button>
+                                                </li>
+                                                <li className="file-queue-item">
+                                                    <span className="name">a.html</span>
+                                                    <span className="meta">12 KB</span>
+                                                    <button type="button" className="file-item-remove">
+                                                        <span>파일 제거</span>
+                                                        <Icon name="crossSmall" width={20} height={20} />
+                                                    </button>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div className="file-actions">
+                                            <button type="button" className="submit">
+                                                <span>첨부하기</span>
+                                                <Icon name="diskArrowRight" width={20} height={20} />
+                                            </button>
+                                            <button type="button" className="cancel">
+                                                <span>전체 취소</span>
+                                                <Icon name="diskXMark" width={20} height={20} />
+                                            </button>
+                                            <p className="file-message"></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
