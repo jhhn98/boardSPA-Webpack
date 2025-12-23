4a4fd70e0f3aac27e815499b84308c4fd3063494
JH0908 <mashifed@gmail.com>
2025-12-23 12:40:21 +0900

    feat: add navigation links to Header

--- front-end/src/components/common/Header.tsx
+++ front-end/src/components/common/Header.tsx
@@ -2,6 +2,10 @@ export default function Header() {
     return (
         <header>
             <h1>BoardSPA</h1>
+            <nav className="navigation">
+                <a href="/">Board1</a>
+                <a href="/">Board2</a>
+            </nav>
         </header>
     )
 }
