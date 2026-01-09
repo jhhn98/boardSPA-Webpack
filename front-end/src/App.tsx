4d0de0de00149865528253adae9e5c3ccf4aaa21
JH0908 <mashifed@gmail.com>
2026-01-09 17:08:22 +0900

    feat(router): add Write and Edit page routing

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
 import { Fragment } from 'react'
 import { Route, Routes } from 'react-router'
 import Layout from './components/common/Layout'
+import Home from './pages/Home'
 import List from './pages/board/List'
 import View from './pages/board/View'
-import Home from './pages/Home'
+import Write from './pages/board/Write'
+import Edit from './pages/board/Edit'
 
 export default function App() {
     return (
@@ -13,6 +15,8 @@ export default function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/bbsList/:bbsNo" element={<List />} />
                     <Route path="/bbsView" element={<View />} />
+                    <Route path="/bbsWrite" element={<Write />} />
+                    <Route path="/bbsEdit" element={<Edit />} />
                 </Route>
             </Routes>
         </Fragment>
